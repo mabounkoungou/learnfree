3b01dfe386253487ab88a52ad807dbf3a6c7298e
raad <raadbouks@gmail.com>
2024-08-22 18:13:29 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+## Changelog
+
+- v1.0.1: Improve Sidebar
+- v1.0.2: Settings Page
---
+++ app/(main)/_components/Logo.tsx
@@ -0,0 +1,7 @@
+export const Logo = ({className}: {className:string}) => {
+  return (
+    <h1 className="font-bold w-full h-full flex items-center justify-center gap-1 text-lg md:text-xl">
+      <span>Learn</span><span className="text-primary">Free</span>
+    </h1>
+  )
+}
